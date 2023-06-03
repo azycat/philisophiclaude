fa5174f8b62f302c4ad59be1ac2032e86b130cf3
Azy <56982188+azycat@users.noreply.github.com>
2023-06-02 19:20:20 -0700

    static frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,48 +7,85 @@ import { Home } from './components/Home'
 import { Department } from './components/Department';
 import { Meepy } from './components/Meepy';
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  return (
-   <BrowserRouter>
-   <div className="container">
+	return (
+        <Routes>
+            <Route path='/' component=
+        </Routes>
+		// <div className="App">
+		// 	<div className="books-home">
+		// 		<div className="App-header">
+		// 			<img src={logo} className="App-logo" alt="logo" />
+		// 			<h1>Philaude</h1>
+		// 		</div>
+		// 		<div className="books-home-content">
+		// 			<div>
+		// 				{/* Book rec shelf*/}
+		// 				<div className="bookshelf">
+		// 					<h2 className="bookshelf-title">Start Reading</h2>
+		// 					<div className="bookshelf-books">
+		// 						<ol className="books-grid">
+		// 							<li>
+		// 								<div className="book">
+		// 									<div classname="book-top">
+		// 									<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+		// 								</div>
+		// 									<div className="book-title">To Kill a Mockingbird</div>
+		// 									<div className="book-author">Harper Lee</div>										
+		// 								</div>
+		// 							</li>
+		// 							<li>
+		// 								<div className="book">
+		// 									<div classname="book-top">
+		// 									<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+		// 								</div>
+		// 									<div className="book-title">To Kill a Mockingbird</div>
+		// 									<div className="book-author">Harper Lee</div>										
+		// 								</div>
+		// 							</li>
+		// 						</ol>
+		// 					</div>
+		// 				</div>
+
+		// 				{/* currently reading */}
+		// 				<div className="bookshelf">
+		// 					<h2 className="bookshelf-title">In Progress</h2>
+		// 					<div className="bookshelf-books">
+		// 						<ol className="books-grid">
+		// 							<li>
+		// 								<div className="book">
+		// 									<div className='book-top'>
+		// 											<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+		// 											<div className="book-progress"> <h3>53%</h3></div>
+		// 									</div>
+		// 									<div className="book-title">to kill a meepingbird</div>
+		// 									<div className="book-author">moopy</div>
+		// 								</div>
+		// 							</li>
+		// 							<li>
+		// 								<div className="book">
+		// 									<div className='book-top'>
+		// 											<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+		// 											<div className="book-progress"> <h3>18%</h3></div>
+		// 									</div>
+		// 									<div className="book-title">to kill a meepingbird</div>
+		// 									<div className="book-author">moopy</div>
+		// 								</div>
+		// 							</li>
+		// 						</ol>
+
+		// 					</div>
 
 
-    <Routes>
-      <Route path='/' element={<Home/>} exact /> # map Home page
-      <Route path='/department' element={<Department/>} exact/>
-      <Route path='/meepy' element={<Meepy/>} exact/>
-    </Routes>
-   </div>
-   </BrowserRouter> 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
+		// 				</div>
 
-      
-
-    //     <button
-    //       onClick={claude.getNewActivity}
-    //       title="Learn More"
-    //       color="#841584"
-    //       accessibilityLabel="Learn more about this purple button"
-    //     />
-
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    // //     </a>
-    //   </header>
-    // </div>
-  );
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// </div>
+	);
 }
 
 export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,17 @@
 import React,{Component} from 'react';
+import BookShelf from './BookShelf';
 
 export class Home extends Component {
     
     // write what needs to be rendered
     render() {
         return(
-            // write html content that needs to be rendered when this component is called
-            <div className="mt-5-d-flex justify-content-left">
-                <h3>welcome to the meepery.
-                    This is the home page.
-                </h3>
+            <div className="books-home">
+                <div className="row bg-dark">
+                    <div className="col-10 App-header">
+                        <h1>Philaude</h1>
+                    </div>
+                </div>
             </div>
         )
     }
